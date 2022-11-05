7c912103acdf87dda01b182d1d8f75420986a306
Ihsanuddin <ihsan624@gmail.com>
2022-11-05 19:20:26 +0700

    migrate #1

--- database/migrations/2022_11_04_071534_create_m_area_table.php
+++ database/migrations/2022_11_04_071534_create_m_area_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('m_area_tables', function (Blueprint $table) {
+        Schema::create('m_area', function (Blueprint $table) {
             $table->id();
             $table->string('m_area_nama');
             $table->bigInteger('m_area_created_by');
             $table->timestamp('m_area_created_at')->useCurrent();
             $table->bigInteger('m_area_updated_by')->nullable();
-            $table->timestamp('m_area_updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->nullable();
+            $table->timestamp('m_area_updated_at')->nullable();
             $table->softDeletes('m_area_deleted_at');
         });
     }
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('m_area_tables');
+        Schema::dropIfExists('m_area');
     }
 };
