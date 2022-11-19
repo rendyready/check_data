8c2a7bd46fe8b1805a22d031c822c820afb9301e
mey <meywul1234@gmail.com>
2022-11-19 10:12:36 +0700

    Seeder mjenisnota fiks

--- database/seeders/MJenisNotaTableSeeder.php
+++ database/seeders/MJenisNotaTableSeeder.php
@@ -150,7 +150,355 @@ class MJenisNotaTableSeeder extends Seeder
                 'm_jenis_nota_nama' => 'Nota A Rungkut ',
                 'm_jenis_nota_group' => 'Nota A',
                 'm_jenis_nota_created_by' => 1
-            ]
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Ambar Ketawang',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Bantul',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Banyudono',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Banyumanik',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Bawen ',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Herritage ',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Boyolali Perintis',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Cilacap',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Ampera',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Tuparev',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Concat Barat',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Concat Timur',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Jakal 8',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Gedong Kuning',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Gonilan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Jakal 12',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Jember',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Jurug',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Karanganyar',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Kediri',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            
+            [
+                'm_jenis_nota_nama' => 'Nota B Kerten',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Klaten Sekarsuli',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Kledokan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Kopral Sayom',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Kudus',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Kulon Progo',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Kupatan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Kyai Mojo ',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Lampersari ',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Madiun',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Magelang',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Perumnas',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Ciliwung',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B La Sucipto',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Sengkaling',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Manahan Barat',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Manahan Timur',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Megatruh',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Monjali',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Muntilan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Ngaliyan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Palagan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Palemsemi',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Pandega',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Pati',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Pattimura',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Pekalongan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Plengkung Gading',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Prambanan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Puri Anjasmoro',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B  GOR Satria',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Wiryatmaja',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            
+            [
+                'm_jenis_nota_nama' => 'Nota B Salatiga Diponegoro',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Salatiga Sudirman',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Sambiroto',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Samirono',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Sampangan',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Sewon',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Solo Baru',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Sompok',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Sragen',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Tegal',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Temanggung',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Tembalang',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Ungaran',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Veteran',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Wonogiri',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Wonosari',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+            [
+                'm_jenis_nota_nama' => 'Nota B Wonosobo',
+                'm_jenis_nota_group' => 'Nota B',
+                'm_jenis_nota_created_by' => 1
+            ],
+
         ]);
     }
 
