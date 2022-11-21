6d24dd9777efa38d1b8dcf1136946bf3524b6f33
Ihsanuddin <ihsan624@gmail.com>
2022-11-21 10:10:15 +0700

    migration rekap void

--- database/migrations/2022_11_09_060982_create_rekap_void_table.php
+++ database/migrations/2022_11_09_060982_create_rekap_void_table.php
@@ -13,37 +13,37 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('m_rekap_void', function (Blueprint $table) {
-            $table->id('m_r_v_id');
-            $table->bigInteger('m_r_v_r_t_id');
-            $table->date('m_r_v_tanggal');
-            $table->smallInteger('m_r_v_shift');
-            $table->time('m_r_v_jam');
-            $table->string('m_r_v_nota_code');
-            $table->bigInteger('m_r_v_m_produk_id');
-            $table->string('m_r_v_m_produk_nama');
-            $table->string('m_r_v_m_menu_cr');
-            $table->string('m_r_v_m_menu_urut');
-            $table->bigInteger('m_r_v_m_jenis_produk_id');
-            $table->string('m_r_v_m_jenis_produk_nama');
-            $table->decimal('m_r_v_m_menu_harga_nominal', 15);
-            $table->integer('m_r_v_qty');
-            $table->decimal('m_r_v_nominal', 15);
-            $table->char('m_r_v_tax_status', 1)->nullable()->default('1');
-            $table->char('m_r_v_sc_status', 1)->nullable()->default('0');
-            $table->string('m_r_v_keterangan');
-            $table->bigInteger('m_r_v_m_w_id');
-            $table->string('m_r_v_m_w_nama');
-            $table->bigInteger('m_r_v_m_area_id');
-            $table->string('m_r_v_m_area_nama');
-            $table->bigInteger('m_r_v_kasir_id');
-            $table->string('m_r_v_kasir_nama');
-            $table->char('m_r_v_status_sync', 1)->default('0');
-            $table->bigInteger('m_r_v_created_by');
-            $table->bigInteger('m_r_v_updated_by')->nullable();
-            $table->timestampTz('m_r_v_created_at')->useCurrent();
-            $table->timestampTz('m_r_v_updated_at')->useCurrentOnUpdate()->nullable()->default(NULL);
-            $table->timestampTz('m_r_v_deleted_at')->nullable()->default(NULL);
+        Schema::create('rekap_void', function (Blueprint $table) {
+            $table->id('r_v_id');
+            $table->bigInteger('r_v_r_t_id');
+            $table->date('r_v_tanggal');
+            $table->smallInteger('r_v_shift');
+            $table->time('r_v_jam');
+            $table->string('r_v_nota_code');
+            $table->bigInteger('r_v_m_produk_id');
+            $table->string('r_v_m_produk_nama');
+            $table->string('r_v_m_menu_cr');
+            $table->string('r_v_m_menu_urut');
+            $table->bigInteger('r_v_m_jenis_produk_id');
+            $table->string('r_v_m_jenis_produk_nama');
+            $table->decimal('r_v_m_menu_harga_nominal', 15);
+            $table->integer('r_v_qty');
+            $table->decimal('r_v_nominal', 15);
+            $table->char('r_v_tax_status', 1)->nullable()->default('1');
+            $table->char('r_v_sc_status', 1)->nullable()->default('0');
+            $table->string('r_v_keterangan');
+            $table->bigInteger('r_v_m_w_id');
+            $table->string('r_v_m_w_nama');
+            $table->bigInteger('r_v_m_area_id');
+            $table->string('r_v_m_area_nama');
+            $table->bigInteger('r_v_kasir_id');
+            $table->string('r_v_kasir_nama');
+            $table->char('r_v_status_sync', 1)->default('0');
+            $table->bigInteger('r_v_created_by');
+            $table->bigInteger('r_v_updated_by')->nullable();
+            $table->timestampTz('r_v_created_at')->useCurrent();
+            $table->timestampTz('r_v_updated_at')->useCurrentOnUpdate()->nullable()->default(NULL);
+            $table->timestampTz('r_v_deleted_at')->nullable()->default(NULL);
         });
     }
 
@@ -54,6 +54,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('m_rekap_void');
+        Schema::dropIfExists('rekap_void');
     }
 };
