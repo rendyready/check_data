7860523cfc30fd57322fcab0a08a15073e9f06ae
Ihsanuddin <ihsan624@gmail.com>
2022-12-08 19:06:50 +0700

    migrate temp transaction

---
+++ app/Models/TmpTransaction.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class TmpTransaction
+ * 
+ * @property uuid $tmp_transaction_id
+ * @property uuid|null $tmp_transaction_id_parent
+ * @property int|null $tmp_transaction_m_t_t_id
+ * @property string|null $tmp_transaction_note_number
+ * @property string|null $tmp_transaction_customer_name
+ * @property time without time zone|null $tmp_transaction_order_time
+ * @property string|null $tmp_transaction_table_list
+ * @property int $tmp_transaction_status
+ * @property string|null $tmp_transaction_reason_status
+ * @property int|null $tmp_transaction_created_by
+ * @property string|null $deleted_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * 
+ * @property Collection|TmpTransactionDetail[] $tmp_transaction_details
+ *
+ * @package App\Models
+ */
+class TmpTransaction extends Model
+{
+	use SoftDeletes;
+	protected $table = 'tmp_transaction';
+	protected $primaryKey = 'tmp_transaction_id';
+	public $incrementing = false;
+
+	protected $casts = [
+		'tmp_transaction_id' => 'uuid',
+		'tmp_transaction_id_parent' => 'uuid',
+		'tmp_transaction_m_t_t_id' => 'int',
+		'tmp_transaction_order_time' => 'time without time zone',
+		'tmp_transaction_table_list' => 'binary',
+		'tmp_transaction_status' => 'int',
+		'tmp_transaction_created_by' => 'int'
+	];
+
+	protected $fillable = [
+		'tmp_transaction_id_parent',
+		'tmp_transaction_m_t_t_id',
+		'tmp_transaction_note_number',
+		'tmp_transaction_customer_name',
+		'tmp_transaction_order_time',
+		'tmp_transaction_table_list',
+		'tmp_transaction_status',
+		'tmp_transaction_reason_status',
+		'tmp_transaction_created_by'
+	];
+
+	public function tmp_transaction_details()
+	{
+		return $this->hasMany(TmpTransactionDetail::class, 'tmp_transaction_detail_tmp_transaction_id');
+	}
+}
---
+++ database/migrations/2022_12_08_074850_create_tmp_transaction_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tmp_transaction', function (Blueprint $table) {
+            $table->uuid('tmp_transaction_id')->default(DB::raw('gen_random_uuid()'))->primary();
+            $table->uuid('tmp_transaction_id_parent')->nullable();
+            $table->bigInteger('tmp_transaction_m_t_t_id')->nullable();
+            $table->string('tmp_transaction_note_number', 18)->nullable()->comment('Note Number');
+            $table->string('tmp_transaction_customer_name', 32)->nullable()->comment('Name Customer or bigboss');
+            $table->time('tmp_transaction_order_time')->nullable()->comment('Order Time');
+            $table->jsonb('tmp_transaction_table_list')->nullable()->comment('List Table in Here');
+            $table->integer('tmp_transaction_status')->default(0)->comment('0 new transaction, 1 not yet paid, 2 get paid, 3 cancel transaction');
+            $table->string('tmp_transaction_reason_status', 128)->nullable()->comment('Reason if cancel transaction');
+            $table->bigInteger('tmp_transaction_created_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            // $table->decimal('tmp_transaction_menu_service_charge', 15)->nullable()->default(0);
+            // $table->decimal('tmp_transaction_tarik_tunai', 15)->nullable()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tmp_transaction');
+    }
+};
