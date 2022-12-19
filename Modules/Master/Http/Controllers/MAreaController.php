7e80ef82bf51e31721506ec72fcde9646ad66bdf
maboxcoffee <maboxcoffe@protonmail.com>
2022-12-19 22:28:01 +0700

    check data insert

--- Modules/Master/Http/Controllers/MAreaController.php
+++ Modules/Master/Http/Controllers/MAreaController.php
@@ -8,8 +8,11 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\MArea;
 use Carbon\Carbon;
+use Error;
 use illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
+
+use function PHPUnit\Framework\returnValue;
 
 class MAreaController extends Controller
 {
@@ -22,41 +25,59 @@ class MAreaController extends Controller
     {
         $data = MArea::select('m_area_id', 'm_area_nama', 'm_area_code')->whereNull('m_area_deleted_at')->orderBy('m_area_id', 'asc')->get();
         return view('master::area', compact('data'));
+        // return $data;
+        // $TE = str::lower(trim('pusat               hjghjg         ')); //trim('             PUsAT    lkjhj    ');
+        // // $tb = DB::table('m_area')->selectRaw('m_area_nama')->whereRaw('LOWER(m_area_nama) =' . "'$TE'")->get();
+        // echo $TE;
+
+        // $value = DB::table('m_area')->whereRaw('LOWER(m_area_nama) =', "{}")->get();
+        // $data = DB::table('m_area')->select('m_area_nama')->where(['m_area_nama' => $value])
+        //     ->get();
+        // $hasil = $data == $pusat;
+        // echo $tb;
+        // if ($tb == "[]")
+        //     echo 'kosong';
+        // else
+        //     echo 'isi';
+        // // var_dump($tb);
+        // echo '</pre>';
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function action(Request $request)
     {
-
         if ($request->ajax()) {
-            $raw = ['m_area_nama' => ['required', 'unique:m_area'],];
-            $value = ['m_area_nama' => Str::lower($request->m_area_nama),];
             if ($request->action == 'add') {
-                $validate = \Validator::make($value, $raw,);
-                if ($validate->fails()) {
-                    return response(['Messages' => 'Data Duplicate !']);
-                } else {
+                $aa = $request->m_area_nama;
+                $aa = str::lower(trim($aa));
+                $tb = DB::table('m_area')->selectRaw('m_area_nama')->whereRaw('LOWER(m_area_nama) =' . "'$aa'")->first();
+
+                if ($tb == "[]") {
                     $data = DB::table('m_area')->insert([
-                        'm_area_nama'    =>  Str::lower($request->m_area_nama),
+                        'm_area_nama'    => Str::upper(trim($request->m_area_nama)),
                         'm_area_code'    =>    $request->m_area_code,
                         'm_area_created_by' => Auth::id(),
                         'm_area_created_at' => Carbon::now(),
                     ]);
+                    return response(['Messages' => 'Congratulations !']);
+                } else {
+                    return response(['Messages' => 'Data Duplicate !']);
                 }
-                return response(['Messages' => 'Congratulations !']);
             } elseif ($request->action == 'edit') {
-                $data = array(
-                    'm_area_nama'    =>    $request->m_area_nama,
-                    'm_area_code'    =>    $request->m_area_code,
-                    'm_area_updated_by' => Auth::id(),
-                    'm_area_updated_at' => Carbon::now(),
-                );
-                DB::table('m_area')->where('m_area_id', $request->id)
-                    ->update($data);
-                return response(['Messages' => 'Data Update !']);
+                $chkEdit = [$request->m_area_nama, $request->m_area_id];
+                $validate = DB::table('m_area')->selectRaw('m_area_id' and 'm_area_nama')->whereRaw('m_area_id and LOWER(m_area_nama) =' . "'$chkEdit'")->toSql();
+                if ($space == false) {
+                    $data = array(
+                        'm_area_nama'    =>    $request->m_area_nama,
+                        'm_area_code'    =>    $request->m_area_code,
+                        'm_area_updated_by' => Auth::id(),
+                        'm_area_updated_at' => Carbon::now(),
+                    );
+                    DB::table('m_area')->where('m_area_id', $request->id)
+                        ->update($data);
+                    return response(['Messages' => 'Data Update !']);
+                } else {
+                    return response(['Messages' => true]);
+                }
             } else {
                 $data = array(
                     'm_area_deleted_at' => Carbon::now(),
@@ -67,7 +88,6 @@ class MAreaController extends Controller
                     ->update($data);
                 return response(['Messages' => 'Data Terhapus !']);
             }
-            return response()->json(['Success' => true]);
         }
     }
 }
