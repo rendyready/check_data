5ffb4a8d00c340fcbfe0b0504a5ee5376633d112
riscodharmawan <riscodharmawan1@gmail.com>
2022-12-21 20:03:57 +0700

    form pembelian add validation

--- database/migrations/2022_12_05_134440_create_rekap_beli_detail_table.php
+++ database/migrations/2022_12_05_134440_create_rekap_beli_detail_table.php
@@ -19,13 +19,16 @@ return new class extends Migration
             $table->bigInteger('rekap_beli_detail_m_produk_id');
             $table->string('rekap_beli_detail_m_produk_code');
             $table->string('rekap_beli_detail_m_produk_nama');
+            $table->string('rekap_beli_detail_catatan');
             $table->decimal('rekap_beli_detail_qty',5,2);
-            $table->string('rekap_beli_detail_satuan')->nullable();
             $table->decimal('rekap_beli_detail_harga',10,2);
             $table->decimal('rekap_beli_detail_disc',8,2)->nullable();
             $table->decimal('rekap_beli_detail_discrp')->nullable();
             $table->decimal('rekap_beli_detail_subtot',9,2);
-            $table->string('rekap_beli_detail_catatan');
+            $table->string('rekap_beli_detail_cht_1')->nullable();
+            $table->string('rekap_beli_detail_satuan_cht_1')->nullable();
+            $table->string('rekap_beli_detail_cht_2')->nullable();
+            $table->string('rekap_beli_detail_satuan_cht_2')->nullable();
             $table->bigInteger('rekap_beli_detail_created_by');
             $table->bigInteger('rekap_beli_detail_updated_by')->nullable();
             $table->bigInteger('rekap_beli_detail_deleted_by')->nullable();
