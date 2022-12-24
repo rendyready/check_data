e9659d343e0446ee225bdd40cb452552db9e973a
maboxcoffee <maboxcoffe@protonmail.com>
2022-12-24 13:57:59 +0700

    fix Waroeng jenis

--- Modules/Master/Http/Controllers/WJenisController.php
+++ Modules/Master/Http/Controllers/WJenisController.php
@@ -17,26 +17,6 @@ class WJenisController extends Controller
     {
         $data = DB::table('m_w_jenis')->whereNull('m_w_jenis_deleted_at')->orderBy('m_w_jenis_id', 'asc')->get();
         return view('master::jenis_waroeng', compact('data'));
-
-
-
-
-
-        // $dafuk = DB::raw('Waroeng Mandiri');
-        // $test = DB::table('m_area')->select('m_area_nama')
-        //     ->where(DB::raw('"m_area_nama"'), '=', "$dafuk")
-        //     ->get();
-
-        // $raw = DB::table('m_w_jenis')
-        //     ->crossJoin('m_w')
-        //     ->selectRaw("m_w_jenis_id,m_w_jenis_nama")->whereRaw("m_w_jenis.m_w_jenis_id= m_w.m_w_m_w_jenis_id")
-        //     ->where(DB::raw('m_w_jenis.m_w_jenis_nama'), '=', "$dafuk")
-        //     ->get();
-        // $test = Str::lower($dafuk);
-        // // $has = $raw = $dafuk;
-        // echo '<pre>';
-        // return var_dump(['data' => $test]);
-        // echo '</pre>';
     }
     public function action(Request $request)
     {
@@ -60,20 +40,23 @@ class WJenisController extends Controller
                     return response(['Messages' => 'Data jenis Waroeng Update !']);
                 }
             } elseif ($request->action == 'edit') {
-                // return 'edit';
                 $raw = $request->m_w_jenis_nama;
-                $editRaw = Str::lower(trim($raw));
-                $editUpdate = DB::table('m_w_jenis')->select('m_w_jenis_nama')
-                    ->whereRaw('LOWER(m_w_jenis_nama)', "'$editRaw'")
-                    ->orderBy('m_w_jenis_nama', 'asc')->first();
-                $nameData = Str::upper(preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $raw));
+                $dataraw = Str::lower(trim($raw));
+                $nameData = Str::lower(preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $raw));
+                $chkEdit = DB::table('m_w_jenis')
+                    ->crossJoin('m_w')
+                    ->selectRaw("m_w_jenis_id,LOWER(m_w_jenis_nama)")
+                    ->whereRaw("m_w_jenis.m_w_jenis_id= m_w.m_w_m_w_jenis_id")
+                    ->where(DB::raw('LOWER(m_w_jenis_nama)'), $nameData)
+                    ->first();
+                 $nameDatap = Str::upper(preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $raw));
                 if (!empty($nameData == null)) {
                     return response(['Messages' => 'Data Tidak Boleh Kosong !']);
-                } elseif ($editUpdate != null) {
+                } elseif ($chkEdit != null) {
                     return response(['Messages' => 'Data Duplicate !']);
-                } else {
+                } elseif ($chkEdit == null) {
                     $data = array(
-                        'm_w_jenis_nama' => $nameData,
+                        'm_w_jenis_nama' => $nameDatap,
                         'm_w_jenis_updated_by' => Auth::id(),
                         'm_w_jenis_updated_at' => Carbon::now(),
                     );
@@ -81,30 +64,24 @@ class WJenisController extends Controller
                         ->update($data);
                     return response(['Messages' => 'Data update !']);
                 }
-                // return response(['Messages' => 'Data error !']);
             } else {
-                $dataChk =  DB::raw('Waroeng Mandiri');
-                $dataChk = Str::lower($dataChk);
-                $delCheck =  DB::table('m_w_jenis')
-                    ->crossJoin('m_w')
-                    ->selectRaw("m_w_jenis_id,m_w_jenis_nama")->whereRaw("m_w_jenis.m_w_jenis_id= m_w.m_w_m_w_jenis_id")
-                    ->where(DB::raw('m_w_jenis.m_w_jenis_nama'), '=', "$dataChk")
-                    ->get();
-                $delCheck = Str::lower($delCheck);
-                if ($delCheck = null) {
+                $dataChk = $request->m_w_jenis_id;
+                $delCheck =  DB::table('m_w')
+                    ->select('m_w_m_w_jenis_id')->where("m_w_m_w_jenis_id", $dataChk)
+                    ->first();
+                if ($delCheck == null) {
                     $data = array(
                         'm_w_jenis_deleted_at' => Carbon::now(),
                         'm_w_jenis_deleted_by' => Auth::id(),
                     );
 
                     DB::table('m_w_jenis')
-                        ->where('m_w_jenis_id', $request->m_w_jenis_id)
+                        ->where('m_w_jenis_id', $dataChk)
                         ->update($data);
                     return response(['Messages' => 'Data Delete !']);
-                } elseif ($delCheck) {
+                } else {
                     return response(['Messages' => 'Tidak Bisa Data Delete !']);
                 }
-                return $delCheck = $delCheck->toSql();
             }
         }
     }
