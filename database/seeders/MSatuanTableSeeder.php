d054d3844e4dfbce9645b4e4f2be86cd4bd51344
riscodharmawan <riscodharmawan1@gmail.com>
2023-02-07 18:12:38 +0700

    add data produk

--- database/seeders/MSatuanTableSeeder.php
+++ database/seeders/MSatuanTableSeeder.php
@@ -17,27 +17,62 @@ class MSatuanTableSeeder extends Seeder
 
         DB::table('m_satuan')->insert([
             [
-                'm_satuan_kode' => 'Kilogram',
+                'm_satuan_kode' => 'kg',
                 'm_satuan_keterangan' =>Null,
                 'm_satuan_created_by' => 1
             ],
             [
-                'm_satuan_kode' => 'Liter',
+                'm_satuan_kode' => 'liter',
                 'm_satuan_keterangan' =>Null,
                 'm_satuan_created_by' => 1
             ],
             [
-                'm_satuan_kode' => 'Biji / Pcs',
+                'm_satuan_kode' => 'biji / pcs',
                 'm_satuan_keterangan' =>Null,
                 'm_satuan_created_by' => 1
             ],
             [
-                'm_satuan_kode' => 'Porsi',
+                'm_satuan_kode' => 'porsi',
                 'm_satuan_keterangan' =>Null,
                 'm_satuan_created_by' => 1
             ],
             [
-                'm_satuan_kode' => 'Paket',
+                'm_satuan_kode' => 'paket',
+                'm_satuan_keterangan' =>Null,
+                'm_satuan_created_by' => 1
+            ],
+            [
+                'm_satuan_kode' => 'pack',
+                'm_satuan_keterangan' =>Null,
+                'm_satuan_created_by' => 1
+            ],
+            [
+                'm_satuan_kode' => 'roll',
+                'm_satuan_keterangan' =>Null,
+                'm_satuan_created_by' => 1
+            ],
+            [
+                'm_satuan_kode' => 'botol',
+                'm_satuan_keterangan' =>Null,
+                'm_satuan_created_by' => 1
+            ],
+            [
+                'm_satuan_kode' => 'galon',
+                'm_satuan_keterangan' =>Null,
+                'm_satuan_created_by' => 1
+            ],
+            [
+                'm_satuan_kode' => 'bks',
+                'm_satuan_keterangan' =>Null,
+                'm_satuan_created_by' => 1
+            ],
+            [
+                'm_satuan_kode' => 'sachet',
+                'm_satuan_keterangan' =>Null,
+                'm_satuan_created_by' => 1
+            ],
+            [
+                'm_satuan_kode' => 'kaleng',
                 'm_satuan_keterangan' =>Null,
                 'm_satuan_created_by' => 1
             ],
