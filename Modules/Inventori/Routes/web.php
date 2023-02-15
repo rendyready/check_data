65d266ceade98ecb84522d2b43385653ac44d7c4
riscodharmawan <riscodharmawan1@gmail.com>
2023-02-15 18:20:41 +0700

    add pecah gabung

--- Modules/Inventori/Routes/web.php
+++ Modules/Inventori/Routes/web.php
@@ -79,3 +79,9 @@ Route::group(['prefix' => 'inventori', 'controller' => MasterBBController::class
     Route::post('m_bb/simpan', 'simpan')->name('simpan.m_bb');
     Route::get('m_bb/list/{id}', 'list')->name('m_bb.list');
 });
+//Pecah Gabung Barang
+Route::group(['prefix' => 'inventori', 'controller' => PecahGabungController::class, 'middleware' => ['auth', 'web']], function () {
+    Route::get('pecah_gabung', 'index')->name('pcb.index');
+    Route::post('m_bb/simpan', 'simpan')->name('simpan.m_bb');
+    Route::get('m_bb/list/{id}', 'list')->name('m_bb.list');
+});
