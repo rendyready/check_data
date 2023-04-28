1f03615ab39959afff471d71886f0a336ceccf4f
Ihsan Mac <ihsanmac@Ihsans-MacBook-Pro.local>
2023-04-28 23:13:03 +0700

    add waiting cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('datasync:cron')->everyMinute();
+        $schedule->command('datasync:cron')->everyMinute()->withoutOverlapping();
         // $schedule->command('inspire')->hourly();
     }
 
