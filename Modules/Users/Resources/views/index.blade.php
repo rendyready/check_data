c4c4e926ff81b7fc59ec1db7437bfb9c34796bfa
riscodharmawan <riscodharmawan1@gmail.com>
2023-06-30 11:24:39 +0700

    perbaikan user duplikat roll n improve perfoma

--- Modules/Users/Resources/views/index.blade.php
+++ Modules/Users/Resources/views/index.blade.php
@@ -110,10 +110,10 @@
                                                                     data-target=".area{{ $val->m_area_id }}">
                                                                     <input type="checkbox"
                                                                         class="css-control-input parentCheckBox pilihSemua"
-                                                                        title="check all"
-                                                                        value="area{{ $val->m_area_id }}">
+                                                                        title="check all" value="area{{ $val->m_area_id }}">
                                                                     <span class="css-control-indicator"></span>
-                                                                    <b class="text-danger">{{ ucwords($val->m_area_nama) }}</b>
+                                                                    <b
+                                                                        class="text-danger">{{ ucwords($val->m_area_nama) }}</b>
                                                                 </label>
                                                                 <ul>
                                                                     @foreach ($val->m_ws as $key2 => $val2)
@@ -166,22 +166,6 @@
                                 </tr>
                             </thead>
                             <tbody id="tablecontents">
-                                @php
-                                    $no = 1;
-                                @endphp
-                                @foreach ($data->users as $item)
-                                    <tr>
-                                        <td>{{ $no++ }}</td>
-                                        <td>{{ $item->username }}</td>
-                                        <td>{{ $item->email }}</td>
-                                        <td>{{ $item->m_w_nama }}</td>
-                                        <td>{{ $item->rolename }}</td>
-                                        <td><a id="buttonEdit" class="btn btn-sm btn-warning buttonEdit"
-                                                value="{{ $item->users_id }}"><i class="fa fa-pencil"></i></a>
-                                            <a class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -207,12 +191,20 @@
                 dropdownPosition: 'below',
                 tags: true,
             });
-            var t = $('#user').DataTable({
-                processing: false,
-                serverSide: false,
-                destroy: true,
-                pageLength: 10,
-                order: [0, 'asc']
+            $('#user').DataTable({
+                "destroy": true,
+                "orderCellsTop": true,
+                "processing": true,
+                "autoWidth": true,
+                "lengthMenu": [
+                    [10, 25, 50, 100, -1],
+                    [10, 25, 50, 100, "All"]
+                ],
+                "pageLength": 10,
+                "ajax": {
+                    "url": "{{route('users.list')}}",
+                    "type": "GET"
+                }
             });
             $("#user").append(
                 $('<tfoot/>').append($("#user thead tr").clone())
@@ -238,7 +230,7 @@
                     dataType: 'json',
                     success: function(respond) {
                         $("#id").val(respond.id).trigger('change');
-                        $("#name").val(respond.name).trigger('change');
+                        $("#name").val(respond.username).trigger('change');
                         $("#email").val(respond.email).trigger('change');
                         $("#roles").val(respond.roles).trigger('change');
                         $("#waroeng_id").val(respond.waroeng_id).trigger('change');
@@ -246,7 +238,7 @@
                             .map(function(item) {
                                 return parseInt(item);
                             });
-                        console.log(waroeng_akses);
+                        // console.log(waroeng_akses);
                         waroeng_akses.forEach(function(value) {
                             var checkbox = document.querySelector('input[value="' +
                                 value + '"]');
