5b65ee6d0ccc8974529cf01efe878fb7656c7660
Ihsanuddin <ihsan624@gmail.com>
2023-07-20 19:23:19 +0700

    add field on tmp

---
+++ database/migrations/2023_07_20_132922_recreate_tmp_transaction_detail.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('tmp_transaction_detail');
+
+        Schema::create('tmp_transaction_detail', function (Blueprint $table) {
+            $table->uuid('tmp_transaction_detail_id')->default(DB::raw('gen_random_uuid()'))->primary();
+            $table->uuid('tmp_transaction_detail_tmp_transaction_id');
+            $table->unsignedBigInteger('tmp_transaction_detail_m_produk_id')->nullable();
+            $table->integer('tmp_transaction_detail_qty')->default(1)->comment('qty menu');
+            $table->bigInteger('tmp_transaction_detail_created_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->integer('tmp_transaction_detail_status')->default(0)->comment('0 new transaction, 1 paid, 2 garansi, 3 ganti baru, 4 gratis');
+            $table->integer('tmp_transaction_detail_qty_approve')->nullable()->comment('qty menu approve');
+            $table->text('tmp_transaction_detail_reasone_approve')->nullable();
+            $table->decimal('tmp_transaction_detail_tax',8,2)->default(0)->comment('percentage tax');
+            $table->decimal('tmp_transaction_detail_service_charge',8,2)->nullable()->comment('percentage percentage charge');
+            $table->decimal('tmp_transaction_detail_discount',8,2)->default(0)->comment('percentage discount');
+            $table->decimal('tmp_transaction_detail_price',15,2)->default(0);
+            $table->decimal('tmp_transaction_detail_nominal',15,2)->default(0);
+            $table->text('tmp_transaction_detail_custom_menu')->nullable();
+            $table->boolean('tmp_transaction_detail_tax_status')->default(true);
+            $table->boolean('tmp_transaction_detail_service_charge_status')->default(true);
+            $table->decimal('tmp_transaction_detail_tax_val', 5,2)->default(0);
+            $table->decimal('tmp_transaction_detail_service_val', 5,2)->default(0);
+            $table->integer('tmp_transaction_detail_production_status')->default(0)->comment('0 not on production, 1 on production, 2 production cancelled');
+            $table->string('tmp_transaction_detail_discount_type')
+            ->comment('Persen,Nominal,Qty,CustomPrice,NormalPrice')
+            ->default('NormalPrice');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tmp_transaction_detail');
+
+        Schema::create('tmp_transaction_detail', function (Blueprint $table) {
+            $table->uuid('tmp_transaction_detail_id')->default(DB::raw('gen_random_uuid()'))->primary();
+            $table->uuid('tmp_transaction_detail_tmp_transaction_id');
+            $table->unsignedBigInteger('tmp_transaction_detail_m_produk_id')->nullable();
+            $table->integer('tmp_transaction_detail_qty')->default(1)->comment('qty menu');
+            $table->bigInteger('tmp_transaction_detail_created_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->integer('tmp_transaction_detail_status')->default(0)->comment('0 new transaction, 1 paid, 2 garansi, 3 ganti baru, 4 gratis');
+            $table->integer('tmp_transaction_detail_qty_approve')->nullable()->comment('qty menu approve');
+            $table->text('tmp_transaction_detail_reasone_approve')->nullable();
+            $table->decimal('tmp_transaction_detail_tax',8,2)->default(0)->comment('percentage tax');
+            $table->decimal('tmp_transaction_detail_service_charge',8,2)->nullable()->comment('percentage percentage charge');
+            $table->decimal('tmp_transaction_detail_discount',8,2)->default(0)->comment('percentage discount');
+            $table->decimal('tmp_transaction_detail_price',15,2)->default(0);
+            $table->decimal('tmp_transaction_detail_nominal',15,2)->default(0);
+            $table->text('tmp_transaction_detail_custom_menu')->nullable();
+            $table->boolean('tmp_transaction_detail_tax_status')->default(true);
+            $table->boolean('tmp_transaction_detail_service_charge_status')->default(true);
+            $table->integer('tmp_transaction_detail_production_status')->default(0)->comment('0 not on production, 1 on production, 2 production cancelled');
+            $table->enum('tmp_transaction_detail_discount_type', ['Persen', 'Nominal', 'Qty', 'CustomPrice'])->default('CustomPrice');
+        });
+    }
+};
