e73f7cf3bb2cdc7c8a80e1b338b788800e2ebdac
ready_rr <rerockchime@gmail.com>
2023-09-06 14:03:06 +0700

    fix rekap tarikan, rekap non menu

--- Modules/Dashboard/Http/Controllers/RekapMenuHarianController.php
+++ Modules/Dashboard/Http/Controllers/RekapMenuHarianController.php
@@ -111,101 +111,66 @@ class RekapMenuHarianController extends Controller
             $refund = $refundM->get();
             $refund2 = $refundM->first();
 
-            // $garansi = DB::table('rekap_garansi')
-            //     ->join('rekap_transaksi', 'r_t_id', 'rekap_garansi_r_t_id')
-            //     ->join('rekap_modal', 'rekap_modal_id', 'r_t_rekap_modal_id')
-            //     ->join('m_transaksi_tipe', 'm_t_t_id', 'r_t_m_t_t_id')
-            //     ->select('rekap_garansi_m_produk_id',
-            //         'rekap_garansi_qty as qty',
-            //         'r_t_tanggal',
-            //         'rekap_modal_sesi',
-            //         'm_t_t_id')
-            //     ->where('rekap_modal_id', $get_modal_id->rekap_modal_id);
-            // $garansi_qty = $garansi->get();
-            // $garansi_notnull = $garansi->first();
-
-            // $garansi_only = DB::table('rekap_garansi')
-            //     ->join('rekap_transaksi', 'r_t_id', 'rekap_garansi_r_t_id')
-            //     ->join('rekap_modal', 'rekap_modal_id', 'r_t_rekap_modal_id')
-            //     ->join('m_transaksi_tipe', 'm_t_t_id', 'r_t_m_t_t_id')
-            //     ->join('m_produk', 'm_produk_id', 'rekap_garansi_m_produk_id')
-            //     ->join('m_jenis_produk', 'm_jenis_produk_id', 'm_produk_m_jenis_produk_id')
-            //     ->select('rekap_garansi_m_produk_id',
-            //         'rekap_garansi_m_produk_nama as menu_gar',
-            //         'rekap_garansi_reguler_price as harga_garansi',
-            //         'rekap_garansi_qty as qty_gar',
-            //         'm_jenis_produk_nama as jenis_produk_gar',
-            //         'r_t_tanggal as tanggal_gar',
-            //         'r_t_m_w_nama as waroeng_gar',
-            //         'rekap_modal_sesi',
-            //         'm_t_t_id',
-            //         'm_t_t_name as tipe_gar'
-            //     )
-            //     ->where('rekap_modal_id', $get_modal_id->rekap_modal_id);
-            // $garansi_only_qty = $garansi_only->get();
-            // $garansi_only_notnull = $garansi_only->first();
-
             $garansi = DB::table('rekap_garansi')
-                ->leftJoin('rekap_transaksi', 'rekap_garansi.rekap_garansi_r_t_id', '=', 'rekap_transaksi.r_t_id')
-                ->leftJoin('m_produk', 'm_produk.m_produk_id', '=', 'rekap_garansi.rekap_garansi_m_produk_id')
-                ->leftJoin('m_jenis_produk', 'm_jenis_produk.m_jenis_produk_id', '=', 'm_produk.m_produk_m_jenis_produk_id')
-                ->leftJoin('m_transaksi_tipe', 'm_transaksi_tipe.m_t_t_id', '=', 'rekap_transaksi.r_t_m_t_t_id')
-                ->leftJoin('rekap_modal', 'rekap_modal.rekap_modal_id', '=', 'rekap_transaksi.r_t_rekap_modal_id')
-                ->whereDate('rekap_modal.rekap_modal_tanggal', $request->tanggal)
-                ->where('rekap_modal.rekap_modal_m_area_id', $request->area)
-                ->where('rekap_modal.rekap_modal_m_w_id', $request->waroeng)
-                ->where('rekap_modal.rekap_modal_id', $get_modal_id->rekap_modal_id);
-
+                ->leftJoin('rekap_transaksi', 'rekap_garansi_r_t_id', '=', 'r_t_id')
+                ->leftJoin('m_produk', 'm_produk_id', '=', 'rekap_garansi_m_produk_id')
+                ->leftJoin('m_jenis_produk', 'm_jenis_produk_id', '=', 'm_produk_m_jenis_produk_id')
+                ->leftJoin('m_transaksi_tipe', 'm_t_t_id', '=', 'r_t_m_t_t_id')
+                ->leftJoin('rekap_modal', 'rekap_modal_id', '=', 'r_t_rekap_modal_id')
+                ->whereDate('rekap_modal_tanggal', $request->tanggal)
+                ->where('rekap_modal_m_area_id', $request->area)
+                ->where('rekap_modal_m_w_id', $request->waroeng)
+                ->where('rekap_modal_id', $get_modal_id->rekap_modal_id);
             if ($request->trans != 'all') {
-                $garansi->where('m_transaksi_tipe.m_t_t_name', $request->trans);
+                $garansi->where('m_t_t_name', $request->trans);
             }
-
-            $garansi = $garansi->selectRaw('max(rekap_garansi.rekap_garansi_qty) AS qty_gar,
-                    max(rekap_garansi.rekap_garansi_reguler_price) as harga_garansi,
-                    rekap_transaksi.r_t_tanggal as tanggal_gar,
-                    rekap_garansi.rekap_garansi_m_produk_nama as menu_gar,
-                    max(rekap_transaksi.r_t_m_w_nama) as waroeng_gar,
-                    max(m_jenis_produk.m_jenis_produk_nama) as jenis_produk_gar,
-                    max(m_transaksi_tipe.m_t_t_name) as tipe_gar,
-                    rekap_modal.rekap_modal_sesi,
-                    max(rekap_garansi.rekap_garansi_price) as r_t_detail_price,
-                    SUM(rekap_garansi.rekap_garansi_nominal) AS nominal_nota,
-                    SUM(rekap_garansi.rekap_garansi_price * rekap_garansi.rekap_garansi_qty) as trans,
-                    SUM(rekap_garansi.rekap_garansi_nominal) - (SUM(rekap_garansi.rekap_garansi_reguler_price * rekap_garansi.rekap_garansi_qty)) cr_trans')
-                ->groupBy('rekap_transaksi.r_t_tanggal',
-                    'rekap_garansi.rekap_garansi_m_produk_nama',
-                    'rekap_modal.rekap_modal_sesi',
+            $garansi = $garansi->selectRaw('
+                    sum(rekap_garansi_qty) AS qty_gar,
+                    rekap_garansi_reguler_price as harga_garansi,
+                    r_t_tanggal as tanggal_gar,
+                    rekap_garansi_m_produk_nama as menu_gar,
+                    r_t_m_w_nama as waroeng_gar,
+                    m_jenis_produk_nama as jenis_produk_gar,
+                    m_t_t_name as tipe_gar,
+                    rekap_modal_sesi
+                    ')
+                ->groupBy(
+                    'harga_garansi',
+                    'tanggal_gar',
+                    'menu_gar',
+                    'waroeng_gar',
+                    'jenis_produk_gar',
+                    'tipe_gar',
+                    'rekap_modal_sesi',
                 )
                 ->orderBy('jenis_produk_gar', 'ASC')
-                ->orderBy('menu_gar', 'ASC')->get();
+                ->orderBy('menu_gar', 'ASC')
+                ->get();
 
-            // return $garansi;
-            // Query untuk data rekap transaksi
             $rekap = DB::table('rekap_transaksi_detail')
-                ->leftJoin('rekap_transaksi', 'rekap_transaksi_detail.r_t_detail_r_t_id', '=', 'rekap_transaksi.r_t_id')
-                ->leftJoin('m_produk', 'm_produk.m_produk_id', '=', 'rekap_transaksi_detail.r_t_detail_m_produk_id')
-                ->leftJoin('m_jenis_produk', 'm_jenis_produk.m_jenis_produk_id', '=', 'm_produk.m_produk_m_jenis_produk_id')
-                ->leftJoin('m_transaksi_tipe', 'm_transaksi_tipe.m_t_t_id', '=', 'rekap_transaksi.r_t_m_t_t_id')
-                ->leftJoin('rekap_modal', 'rekap_modal.rekap_modal_id', '=', 'rekap_transaksi.r_t_rekap_modal_id')
-                ->whereDate('rekap_modal.rekap_modal_tanggal', $request->tanggal)
-                ->where('rekap_modal.rekap_modal_m_area_id', $request->area)
-                ->where('rekap_modal.rekap_modal_m_w_id', $request->waroeng)
-                ->where('rekap_modal.rekap_modal_id', $get_modal_id->rekap_modal_id);
-
+                ->leftJoin('rekap_transaksi', 'r_t_id', 'r_t_detail_r_t_id')
+                ->leftJoin('m_w', 'm_w_id', 'r_t_m_w_id')
+                ->leftJoin('m_produk', 'm_produk_id', 'r_t_detail_m_produk_id')
+                ->leftJoin('m_jenis_produk', 'm_jenis_produk_id', 'm_produk_m_jenis_produk_id')
+                ->leftJoin('m_transaksi_tipe', 'm_t_t_id', 'r_t_m_t_t_id')
+                ->leftJoin('rekap_modal', 'rekap_modal_id', 'r_t_rekap_modal_id')
+                ->whereDate('rekap_modal_tanggal', $request->tanggal)
+                ->where('rekap_modal_m_area_id', $request->area)
+                ->where('rekap_modal_m_w_id', $request->waroeng)
+                ->where('rekap_modal_id', $get_modal_id->rekap_modal_id);
             if ($request->trans != 'all') {
-                $rekap->where('m_transaksi_tipe.m_t_t_name', $request->trans);
+                $rekap->where('m_t_t_name', $request->trans);
             }
 
-            $rekap = $rekap->selectRaw('SUM(r_t_detail_qty) AS qty,
+            $rekap = $rekap->selectRaw('
+                    SUM(r_t_detail_qty) AS qty,
                     r_t_detail_reguler_price,
                     r_t_tanggal,
                     r_t_detail_m_produk_nama,
                     r_t_detail_m_produk_id,
-                    r_t_m_w_nama as m_w_nama,
-                    m_jenis_produk_id,
+                    m_w_nama, m_jenis_produk_id,
                     m_jenis_produk_nama,
-                    m_t_t_name,
-                    m_t_t_id,
+                    m_t_t_name, m_t_t_id,
                     rekap_modal_sesi,
                     r_t_detail_price,
                     SUM(r_t_detail_nominal) AS nominal_nota,
@@ -213,13 +178,25 @@ class RekapMenuHarianController extends Controller
                     SUM(r_t_detail_nominal) - (SUM(r_t_detail_reguler_price * r_t_detail_qty)) cr_trans,
                     sum(r_t_detail_nominal_pajak) pajak,
                     r_t_detail_package_price as kemasan,
-                    max(rekap_transaksi.r_t_status) r_t_status')
-                ->groupBy('rekap_transaksi.r_t_tanggal', 'rekap_transaksi_detail.r_t_detail_m_produk_nama', 'rekap_transaksi_detail.r_t_detail_m_produk_id', 'rekap_transaksi.r_t_m_w_nama', 'rekap_transaksi_detail.r_t_detail_reguler_price', 'm_jenis_produk.m_jenis_produk_nama', 'm_jenis_produk.m_jenis_produk_id', 'm_transaksi_tipe.m_t_t_name', 'm_transaksi_tipe.m_t_t_id', 'rekap_modal.rekap_modal_sesi', 'rekap_transaksi_detail.r_t_detail_price', 'rekap_transaksi.r_t_id', 'rekap_transaksi_detail.r_t_detail_package_price')
-                ->orderBy('m_jenis_produk.m_jenis_produk_id', 'ASC')
-                ->orderBy('rekap_transaksi_detail.r_t_detail_m_produk_nama', 'ASC')->get();
-
-            // Gabungkan hasil query
-            // $result = $rekap->union($garansi)->get();
+                    max(r_t_status) r_t_status
+                    ')
+                ->groupBy(
+                    'r_t_tanggal',
+                    'r_t_detail_m_produk_nama',
+                    'r_t_detail_m_produk_id',
+                    'm_w_nama',
+                    'r_t_detail_reguler_price',
+                    'm_jenis_produk_nama',
+                    'm_jenis_produk_id',
+                    'm_t_t_name',
+                    'm_t_t_id',
+                    'rekap_modal_sesi',
+                    'r_t_detail_price',
+                    'kemasan'
+                )
+                ->orderBy('m_jenis_produk_id', 'ASC')
+                ->orderBy('r_t_detail_m_produk_nama', 'ASC')
+                ->get();
 
         } else {
             return response()->json(['messages' => 'Data pada tanggal ' . $request->tanggal . ' tidak ada', 'type' => 'error']);
@@ -232,28 +209,10 @@ class RekapMenuHarianController extends Controller
         $totalselisihCR = 0;
         $totalPajak = 0;
         $totalselisihTax = 0;
+        $totalNominal_garansi = 0;
         $data = array();
-
-        // foreach ($garansi as $key => $valGaransi) {
-        //     // if ($val_menu->r_t_detail_m_produk_id == $valGaransi->rekap_garansi_m_produk_id && $val_menu->r_t_tanggal == $valGaransi->r_t_tanggal && $val_menu->m_t_t_id == $valGaransi->m_t_t_id && $val_menu->id == $valGaransi->id) {
-        //     $nominal_garansi = $valGaransi->harga_garansi + $valGaransi->qty_gar;
-        //     // $row[] = date('d-m-Y', strtotime($valGaransi->tanggal_gar));
-        //     // $row[] = $valGaransi->waroeng_gar;
-        //     $row[] = $valGaransi->menu_gar;
-        //     $row[] = $valGaransi->qty_gar;
-        //     $row[] = number_format($nominal_garansi);
-        //     // $row[] = $valGaransi->jenis_produk_gar;
-        //     // $row[] = $valGaransi->tipe_gar;
-        //     // $pajakMenu = $pajakMenu + (($val_menu->r_t_detail_reguler_price * $valGaransi->qty) * 0.1);
-        //     // return $valGaransi->menu_gar;
-        //     // }
-        // }
-
-        // return $row;
-
         foreach ($garansi as $key => $valGaransi) {
-            // if ($val_menu->r_t_tanggal == $valGaransi->tanggal_gar && $val_menu->id == $valGaransi->id) {
-            $nominal_garansi = $valGaransi->harga_garansi + $valGaransi->qty_gar;
+            $nominal_garansi = $valGaransi->harga_garansi * $valGaransi->qty_gar;
             $garansi_row = array();
             $garansi_row[] = date('d-m-Y', strtotime($valGaransi->tanggal_gar));
             $garansi_row[] = $valGaransi->waroeng_gar;
@@ -262,45 +221,19 @@ class RekapMenuHarianController extends Controller
             $garansi_row[] = number_format($nominal_garansi);
             $garansi_row[] = $valGaransi->jenis_produk_gar;
             $garansi_row[] = $valGaransi->tipe_gar;
-            $garansi_row[] = 'garansi';
             $garansi_row[] = '';
             $garansi_row[] = '';
             $garansi_row[] = '';
+            $garansi_row[] = 'Menu Garansi';
             $garansi_row[] = '';
             $garansi_row[] = '';
             $garansi_row[] = '';
-            // $pajakMenu = $pajakMenu + (($val_menu->r_t_detail_reguler_price * $valGaransi->qty) * 0.1);
-            // return $valGaransi->menu_gar;
             $data[] = $garansi_row;
-            // }
+
+            $totalNominal_garansi += $nominal_garansi;
         }
+
         foreach ($rekap as $key => $val_menu) {
-            // $garansi = DB::table('rekap_garansi')
-            //     ->leftJoin('rekap_transaksi', 'rekap_garansi_r_t_id', '=', 'r_t_id')
-            //     ->leftJoin('m_produk', 'm_produk_id', '=', 'rekap_garansi_m_produk_id')
-            //     ->leftJoin('m_jenis_produk', 'm_jenis_produk_id', '=', 'm_produk_m_jenis_produk_id')
-            //     ->leftJoin('m_transaksi_tipe', 'm_t_t_id', '=', 'r_t_m_t_t_id')
-            //     ->select(
-            //         'rekap_garansi_r_t_id as id',
-            //         'rekap_garansi_m_produk_nama as menu',
-            //         'rekap_garansi_reguler_price as harga',
-            //         'rekap_garansi_qty as qty',
-            //         'm_jenis_produk_nama',
-            //         'm_t_t_name'
-            //     )
-            //     ->where('rekap_garansi_r_t_id', $val_menu->id)
-            //     ->first();
-
-            // if (!empty($garansi) && $val_menu->id == $garansi->id) {
-            //     $garansi_row[] = $garansi->menu;
-            //     $garansi_row[] = $garansi->qty;
-            //     $garansi_row[] = number_format($garansi->harga);
-            //     $garansi_row[] = $garansi->m_jenis_produk_nama;
-            //     $garansi_row[] = $garansi->m_t_t_name;
-
-            //     $data[] = $row;
-            //     $data[] = $garansi_row;
-            // } else {
             $row = array();
             $row[] = date('d-m-Y', strtotime($val_menu->r_t_tanggal));
             $row[] = $val_menu->m_w_nama;
@@ -371,13 +304,13 @@ class RekapMenuHarianController extends Controller
             $totalPajak += $pajak;
             $totalselisihTax += $selisihTax;
         }
-        // }
+
         $totalRow = array();
         $totalRow[] = '';
         $totalRow[] = '';
         $totalRow[] = 'Total';
         $totalRow[] = '';
-        $totalRow[] = number_format($totalNominal);
+        $totalRow[] = number_format($totalNominal + $totalNominal_garansi);
         $totalRow[] = '';
         $totalRow[] = '';
         $totalRow[] = number_format($totalNominal_trans);
