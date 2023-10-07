82a50f50fe5984834cae42a39ce9aa0f1ed3680e
Ihsanuddin <ihsan624@gmail.com>
2023-10-07 16:18:33 +0700

    migrate rev

--- database/migrations/2023_09_25_120131_add_time_rekap_transaksi.php
+++ database/migrations/2023_09_25_120131_add_time_rekap_transaksi.php
@@ -13,22 +13,47 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('tmp_transaction', function (Blueprint $table) {
-            $table->dateTime('tmp_transaction_qrcode_datetime')->nullable();
-            // $table->time('tmp_transaction_kitchen_order_datetime')->nullable();
-            $table->time('tmp_transaction_kitchen_done_time')->nullable();
-        });
-        Schema::table('rekap_transaksi', function (Blueprint $table) {
-            $table->dateTime('r_t_qrcode_datetime')->nullable();
-            $table->dateTime('r_t_tmpcr_datetime')->nullable();
-            // $table->time('r_t_kitchen_order_time')->nullable();
-            $table->time('r_t_kitchen_done_time')->nullable();
-
-        });
-
-        Schema::table('m_w', function (Blueprint $table) {
-            $table->string('m_w_telp')->nullable();
-        });
+        if (!Schema::hasColumn('tmp_transaction', 'tmp_transaction_qrcode_datetime')) {
+            Schema::table('tmp_transaction', function (Blueprint $table) {
+                $table->dateTime('tmp_transaction_qrcode_datetime')->nullable();
+            });
+        }
+        if (!Schema::hasColumn('tmp_transaction', 'tmp_transaction_kitchen_done_time')) {
+            Schema::table('tmp_transaction', function (Blueprint $table) {
+                $table->time('tmp_transaction_kitchen_done_time')->nullable();
+            });
+        }
+        // Schema::table('tmp_transaction', function (Blueprint $table) {
+        //     $table->dateTime('tmp_transaction_qrcode_datetime')->nullable();
+        //     // $table->time('tmp_transaction_kitchen_order_datetime')->nullable();
+        //     $table->time('tmp_transaction_kitchen_done_time')->nullable();
+        // });
+        if (!Schema::hasColumn('rekap_transaksi', 'r_t_qrcode_datetime')) {
+            Schema::table('rekap_transaksi', function (Blueprint $table) {
+                $table->dateTime('r_t_qrcode_datetime')->nullable();
+            });
+        }
+        if (!Schema::hasColumn('rekap_transaksi', 'r_t_tmpcr_datetime')) {
+            Schema::table('rekap_transaksi', function (Blueprint $table) {
+                $table->dateTime('r_t_tmpcr_datetime')->nullable();
+            });
+        }
+        if (!Schema::hasColumn('rekap_transaksi', 'r_t_kitchen_done_time')) {
+            Schema::table('rekap_transaksi', function (Blueprint $table) {
+                $table->time('r_t_kitchen_done_time')->nullable();
+            });
+        }
+        // Schema::table('rekap_transaksi', function (Blueprint $table) {
+        //     $table->dateTime('r_t_qrcode_datetime')->nullable();
+        //     $table->dateTime('r_t_tmpcr_datetime')->nullable();
+        //     // $table->time('r_t_kitchen_order_time')->nullable();
+        //     $table->time('r_t_kitchen_done_time')->nullable();
+        // });
+        if (!Schema::hasColumn('m_w', 'm_w_telp')) {
+            Schema::table('m_w', function (Blueprint $table) {
+                $table->string('m_w_telp')->nullable();
+            });
+        }
     }
 
     /**
