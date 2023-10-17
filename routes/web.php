fd1ff4ef6fd24bb374ae8fbb96201027005d3f88
ready_rr <rerockchime@gmail.com>
2023-10-17 13:32:10 +0700

    rekap tarikan sub total plus kemasan

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,8 @@ Route::post('/users/pass/update', [App\Http\Controllers\Auth\LoginController::cl
     ->middleware('web')
     ->name('password.changes');
 Route::get('/users/noakses', [App\Http\Controllers\Auth\LoginController::class, 'no_akses'])->name('users.noakses');
+Route::get('/count-data', [App\Http\Controllers\ControlDataController::class, 'CountData']);
+
 // Dashboard Route
 Route::middleware(['auth', 'web'])->group(function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
